ba2b03831edf6fbd5cda0969251fc5b196e638df
=houfulin <houfulin@qq.com>
2023-06-04 15:19:34 +0800

    axios基础封装

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+//测试接口函数
+import { getCategory } from '@/APIS/testAPI'
+getCategory().then(res => {
+    console.log(res);
+})
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')

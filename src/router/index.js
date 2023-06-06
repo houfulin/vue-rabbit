60af7ac8aef25c1d956debfe2902ea560899665d
=houfulin <houfulin@qq.com>
2023-06-07 00:32:45 +0800

    feat：详情页和商品展示区

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,8 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
+import Detail from '@/views/Detail/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,8 +22,16 @@ const router = createRouter({
           component: Home
         },
         {
-          path: 'category',
+          path: 'category/:id',
           component: Category
+        },
+        {
+          path: 'category/sub/:id',
+          component: SubCategory
+        },
+        {
+          path: 'Detail/:id',
+          component: Detail
         },
       ]
     },
@@ -30,7 +40,14 @@ const router = createRouter({
       component: Login
 
     }
-  ]
+  ],
+  //路由滚动行为定制
+  scrollBehavior() {
+    return {
+      top: 0
+    }
+    // ...
+  }
 })
 
 export default router

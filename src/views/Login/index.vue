7875f21d0a0da34e808bac117da7ea4123ac7c50
=houfulin <houfulin@qq.com>
2023-06-07 20:19:24 +0800

    基础登录实现

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,9 @@
 <script setup>
 import { ref } from "vue";
+import { loginAPI } from "@/APIS/user.js";
+import { ElMessage } from "element-plus";
+import "element-plus/theme-chalk/el-message.css";
+import { useRouter } from "vue-router";
 //表单校验（账号名+密码）
 //1.准备表单对象
 const form = ref({
@@ -47,13 +51,20 @@ const rules = {
 
 //3.获取form实例做统一校验
 const formRef = ref(null);
+const router = useRouter();
 const doLogin = () => {
+  const { account, password } = form.value;
   //调用实例方法
-  formRef.value.validate((valid) => {
+  formRef.value.validate(async (valid) => {
     //valid:所有表单校验都通过 才为true
     //以valid作为判断条件 如果通过校验才执行登录逻辑
-    if (calid) {
+    if (valid) {
       //TODO LOGIN
+      const res = await loginAPI({ account, password });
+      //1.提示用户
+      ElMessage({ type: "success", message: "登陆成功" });
+      //2.跳转首页
+      router.replace({ path: "/" });
     }
   });
 };

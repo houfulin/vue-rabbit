8550a7a445028176061ce1f0210ced24b40da4e2
=houfulin <houfulin@qq.com>
2023-06-08 19:19:15 +0800

    本地购物车-购物车列表-全选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -35,16 +35,24 @@ export const useCartStore = defineStore('cart', () => {
         const item = cartList.value.find((item) => item.skuId === skuId)
         item.selected = selected
     }
+    //全选功能
+    const allCheck = (selected) => {
+        cartList.value.forEach(item => item.selected = selected)
+    }
     //计算属性
     //1.  所有项的count之和
     const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
     //2.总价
     const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
+    //是否全选
+    const isAll = computed(() => cartList.value.every((item) => item.selected))
     return {
         cartList,
         addCart,
         delCart,
+        isAll,
         allCount,
+        allCheck,
         allPrice,
         singleCheck
     }

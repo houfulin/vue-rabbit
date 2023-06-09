2aaadb95340024728b8d638b796a31a3fe910ae8
=houfulin <houfulin@qq.com>
2023-06-09 17:02:36 +0800

    结算页-提交订单业务实现

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -1,7 +1,11 @@
 <script setup>
 import { getCheckoutInfoAPI } from "@/APIS/checkout.js";
-
+import { crateOrderAPI } from "@/APIS/checkout.js";
 import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { useCartStore } from "@/stores/cartStore";
+const carStore = useCartStore();
+const router = useRouter();
 
 const checkInfo = ref({}); // 订单对象
 const curAddress = ref({});
@@ -26,6 +30,31 @@ const switchAddress = (item) => {
 const confirm = () => {
   curAddress.value = activeAddress.value;
   showDialog.value = false;
+};
+
+//创建订单
+const createOrder = async () => {
+  const res = await crateOrderAPI({
+    delivertTimeType: 1,
+    payType: 1,
+    payChannel: 1,
+    buyerMessage: "",
+    goods: checkInfo.value.goods.map((item) => {
+      return {
+        skuId: item.skuId,
+        count: item.count,
+      };
+    }),
+    addressId: curAddress.value.id,
+  });
+  const orderId = res.result.id;
+  router.push({
+    path: "/pay",
+    query: {
+      id: orderId,
+    },
+  });
+  carStore.updateNewList();
 };
 </script>
 
@@ -136,7 +165,9 @@ const confirm = () => {
         </div>
         <!-- 提交订单 -->
         <div class="submit">
-          <el-button type="primary" size="large">提交订单</el-button>
+          <el-button type="primary" size="large" @click="createOrder"
+            >提交订单</el-button
+          >
         </div>
       </div>
     </div>
